fb27f68dad71bc075396340fb3e3e670b4982798
Carlos Contreras <47269053+carloscontrerasherrera@users.noreply.github.com>
2020-09-20 09:52:37 +0200

    Add files via upload

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 $plugin->component = 'block_control_sesion';
-$plugin->version = 2020081801;  // versión del bloque por fecha/hora -> YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2020091901;  // versión del bloque por fecha/hora -> YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.0-r1';
